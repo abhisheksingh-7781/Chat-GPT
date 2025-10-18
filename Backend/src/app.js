f8012140fad66844bc02fcf41bd56de7a4a3e06f
Abhishek@7781 <abhisheksingh5208@gmail.com>
2025-10-18 18:28:32 +0530

    feat: Initialize frontend project with Vite, React, and routing
    - Added package.json with dependencies for React, React Router, and Vite.
    - Included Vite logo SVG in public directory.
    - Created App component with routing setup.
    - Implemented AppRoutes for navigation between Home, Register, and Login pages.
    - Developed Home, Register, and Login pages with basic structure and form handling.
    - Added ThemeToggle component for light/dark mode switching.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,10 +2,17 @@ const express=require("express")
 const authRoutes=require("../src/routes/user.routes")
 const chatRouter=require('../src/routes/chat.routes')
 const cookieParser=require("cookie-parser")
+const cors=require("cors")
+
+
 const app=express()
 
 
 app.use(express.json())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 app.use(cookieParser())
 
 app.use("/api/auth",authRoutes)
